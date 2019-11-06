693ea5602de768896e809280317a2e6b59e80b2a
Eivind Lie Andreassen <ela4991@gmail.com>
2019-11-06 14:59:38 +0100

    Add legal move check for rook

--- chess.py
+++ chess.py
@@ -1,14 +1,66 @@
-from copy import deepcopy
 import string
+import numpy as np
+
 
 def create_board():
-    return [
+    return np.array([
         ['WR', 'WN', 'WB', 'WQ', 'WK', 'WB', 'WN', 'WR'],
         ['WP']*8] + \
         [['']*8]*4 \
         + [['BP']*8,
         ['BR', 'BN', 'BB', 'BQ', 'BK', 'BB', 'BN', 'BR'],
-    ]
+    ])
+
+
+def coords_to_tile(x, y):
+    assert 0 < x < 8 and 0 < y < 8, 'Invalid coordinates'
+    letter = chr(x + ord('a'))
+    tile = letter + str(y + 1)
+    return tile
+
+
+def tile_to_coords(tile):
+    letter = tile[0].lower()
+    x = ord(letter) - ord('a')
+    y = int(tile[1]) - 1
+    assert 0 <= x < 8 and 0 <= y < 8, 'Invalid tile'
+    return x, y
+
+
+def translate_move():
+    pass
+
+
+def is_legal_move(board, from_tile, to_tile):
+    from_x, from_y = tile_to_coords(from_tile)
+    to_x, to_y = tile_to_coords(to_tile)
+
+    if not all(0 <= e < 8 for e in (from_x, from_y, to_x, to_y)):
+        return False
+
+    tile_value = board[from_y, from_x]
+    if not tile_value:
+        return False
+    color, piece = tile_value
+
+    if piece == 'R':
+        if from_x == to_x:
+            return np.all(board[to_y:from_y:np.sign(from_y - to_y), from_x] == '')
+        elif from_y == to_y:
+            return np.all(board[from_y, to_x:from_x:np.sign(from_x - to_x)] == '')
+    elif piece == 'N':
+        pass
+    elif piece == 'B':
+        pass
+    elif piece == 'Q':
+        pass
+    elif piece == 'K':
+        pass
+    elif piece == 'P':
+        pass
+    else:
+        assert False, 'Invalid piece!'
+    return False
 
 
 def get_piece_symbol(piece_code):
@@ -29,4 +81,6 @@ def print_board(board):
 
 if __name__ == '__main__':
     board = create_board()
+    board[1, 0] = ''
     print_board(board)
+    print(is_legal_move(board, 'a1', 'a3'))
